5ce7d0f7b484f9879d55b30dc58b9c6e78082300
surajrastogi1 <surajrastogi606@gmail.com>
2026-07-07 06:22:46 +0530

    Register-Use-featurer

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from sqlmodel import SQLModel,create_engine,Field,Session
+from fastapi import FastAPI,HTTPException,Depends
+from sqlmodel import SQLModel,create_engine,Field,Session,select
+from pydantic import EmailStr
 
 class User(SQLModel,table=True):
     id: int | None = Field(default=None,primary_key=True)
@@ -7,14 +8,25 @@ class User(SQLModel,table=True):
     email : str = Field(unique=True)
     password : str 
 
+class UserCreate(SQLModel): #this helps to user enter these things
+    username: str
+    email: EmailStr
+    password: str
+
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 #this talks to database
 engine = create_engine(sqlite_url,connect_args={"check_same_thread" : False})
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+def get_session():  #to open and close DB
+    with Session(engine) as session:
+        yield session
 
 app = FastAPI(title="ResearchMind")
 
@@ -25,4 +37,25 @@ def on_startup():
 @app.get("/")
 def root():
     return {"message" : "API Running and Database Connected"}
+
+@app.post("/register")
+def register(user_data : UserCreate, session : Session = Depends(get_session)):
+    #1. check if username already exists
+    existing_user = session.exec(select(User).where(User.username == user_data.username)).first()
+    if existing_user:
+        raise HTTPException(status_code=400,detail="Username already exists")
+    #2. check if email already exists
+    existing_email = session.exec(select(User).where(User.email == user_data.email)).first()
+    if existing_email:
+        raise HTTPException(status_code=400,detail="Email already exists")
+    
+    new_user = User(
+        username=user_data.username,
+        email = user_data.email,
+        password = user_data.password
+    )
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
 
